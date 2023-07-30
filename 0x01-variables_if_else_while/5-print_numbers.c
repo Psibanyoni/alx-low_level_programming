0ddce0ad907bb221a34faa6d4df6b3c198231d39
Portia Sibanyoni <psibanyoni@icloud.com>
2023-07-30 15:13:35 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Prog Entry point
- * Return: 0
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a;
+	int n;
 
-	for (a = 0; a < 10; a++)
-		printf("%d", a);
-	putchar('\n');
-	return:(0)
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
 }
