7ace3ed7ba5af5ef1091ef62c4c1cac7b2d48cae
Portia Sibanyoni <psibanyoni@icloud.com>
2023-08-03 07:07:14 -0700

    100-is_palindrome.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,23 @@
 #include "main.h"
 /**
  * main - Determines if a number is positive, negative or zero.
+ * 0: is the number to be checked
  * Return: 0
  */
 void positive_or_negative(int i)
 {
 	if (i < 0)
-		printf("%d is positive\n", i);
-
-	else if (i < 0)
-		printf("%d is negative\n", i);
-
+	{
+		printf("%d is %s\n", 1, "negative");
+	}
+	else if (i > 0)
+	{
+		printf("%d is %s\n", 1, "positive",);
+	}
 	else
-		printf("%d is zero\n", i);
+	{
+		printf("%d is %s\n", 1, "zero",);
+	}
+	return;
+
 }
