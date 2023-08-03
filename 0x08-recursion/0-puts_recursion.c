cec2dea6b86186d9cd547136253e99c09b13e2d0
Portia Sibanyoni <psibanyoni@icloud.com>
2023-08-03 05:23:28 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,21 @@
 #include "main.h"
+
 /**
- * _puts-recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * _puts_recursion - recursively prints a string
+ * @s: the string to be printed
+ *
+ * Author: Destiny Saturday (DestinedCodes)
+ * Date: 03/08/2023
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
+	{
 		_putchar('\n');
+	}
 }
