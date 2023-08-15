53ccd0cbe249d9e1d229e99566fda76764b4d9c8
Portia Sibanyoni <psibanyoni@icloud.com>
2023-08-15 06:25:39 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: size of array command line arg.
+ * @argv: command line argument
+ *
+ * Return: Always 0, success
+ *
+ * Author: Portia Sibanyoni
+ * Date: 15/06/2023
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
